488d544de438334ae4dd248a114c356894606e9b
Joel Tannas <jtannas@gmail.com>
2018-03-10 11:33:17 -0700

    add url slugs to the tags table (#6)

--- db/migrations/20180309153031_create_tags.js
+++ db/migrations/20180309153031_create_tags.js
@@ -1,11 +1,13 @@
 exports.up = async function(knex, Promise) {
   return knex.schema.createTable("tags", function(table) {
     table.increments('id').primary();
-    table.text('name').notNullable();
+    table.text('display_name').notNullable();
+    table.text('url_slug').notNullable();
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
 
-    table.unique('name')
+    table.unique('display_name');
+    table.unique('url_slug');
   });
 };
 
--- db/seeds/20180309153031_tags.js
+++ db/seeds/20180309153031_tags.js
@@ -4,9 +4,9 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('tags').insert([
-        { id: 1, name: 'tag_name_1' },
-        { id: 2, name: 'tag_name_2' },
-        { id: 3, name: 'tag_name_3' }
+        { id: 1, display_name: 'tag_name_1', url_slug: 'url_slug_1' },
+        { id: 2, display_name: 'tag_name_2', url_slug: 'url_slug_2' },
+        { id: 3, display_name: 'tag_name_3', url_slug: 'url_slug_3' }
       ]);
     });
 };
