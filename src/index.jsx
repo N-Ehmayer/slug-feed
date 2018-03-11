1bf032026981c76bab217bc4d70ed1496ae7c600
Spencer Greff <34141250+Spezp@users.noreply.github.com>
2018-03-10 19:25:16 -0700

    Material Design Bootstrap / React w/ webpack (#10)
    * Created working react component. Fixed webpack bug. Rolled back react-dom & react versions. Bootstrap in styles dir.
    
    * Rebase project-setup/styling with master
    
    * Merge conflict resolution
    
    * Implemented Material design UI

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+require("../styles/application.scss");
+
+// Bootstrap.
+require('script-loader!uglify-loader!../styles/js/bootstrap.js');
+
+// MDB modules
+//require("font-awesome/css/font-awesome.min.css");
+//require("mdbreact/docs/css/mdb.min.css");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+const root = document.getElementById('react-root');
+
+ReactDOM.render(<App/>, root);
+
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const config = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  resolve: {
+    alias: {
+      mdbreact: path.resolve('./src/index')
+    }
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel-loader', query: { presets: ['react', 'es2015'] } },
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
+      { test: /\.png(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/png' }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
+};
+
+module.exports = config;
