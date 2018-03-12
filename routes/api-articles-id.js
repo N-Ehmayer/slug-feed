eb49907452ab1a2122569cd9df0fb550c911595b
Joel Tannas <jtannas@gmail.com>
2018-03-12 17:25:53 -0600

    add data-helper for retrieving article data

--- routes/api-articles-id.js
+++ routes/api-articles-id.js
@@ -1,71 +1,15 @@
 "use strict";
+const getArticle = require('./helpers/article');
 
 function appendRoutes(router, knex) {
 
-  router.get('/api/articles/:id', (req, res) => {
-    let articleObject = {};
-
-    const articleQuery = knex
-      .select('*')
-      .from('articles')
-      .where('id', req.params.id)
-      .orderBy('created_at');
-
-    const sectionsQuery = knex
-      .select('*')
-      .from('sections')
-      .where('article_id', req.params.id)
-      .orderBy('id');
-
-    const tagsQuery = knex
-      .select('tags.*')
-      .from('article_tags')
-      .join('tags', 'tags.id', 'article_tags.tag_id')
-      .where('article_tags.article_id', req.params.id)
-      .orderBy('tags.id');
-
-    const sectionCommentsQuery = function(sectionId) {
-      return knex
-        .select('comments.*')
-        .from('comments')
-        .where('section_id', sectionId)
-        .orderBy('created_at');
-    };
-
-    const authorQuery = knex
-      .select('users.*')
-      .from('users').join('articles', 'users.id', 'articles.user_id')
-      .where('articles.id', req.params.id);
-
-    articleQuery
-      .then(articles => articleObject = articles[0])
-      .catch(error => {
-        res.status(500).send('Error 500: We messed up getting your article');
-        console.error(error);
-      }).then(() =>
-        sectionsQuery.then(sections => {
-          articleObject.sections = sections;
-          return Promise.all(sections.map(section => {
-            return sectionCommentsQuery(section.id)
-              .then(comments => section.comments = comments);
-          }));
-        })
-      ).catch(error => {
-        res.status(500).send('Error 500: We messed up getting your article sections');
-        console.error(error);
-      }).then(() => {
-        return tagsQuery.then(tags => { articleObject.tags = tags; });
-      }).catch(error => {
-        res.status(500).send('Error 500: We messed up getting your article tags');
-        console.error(error);
-      }).then(() => res.json(articleObject))
-      .catch(error => {
-        res.status(500).send('Error 500: We messed up sending your article info');
-        console.error(error);
-      });
+  router.get('/api/articles/:id', (request, response) => {
+    getArticle(knex, request.params.id, article => {
+      response.json(article)
+    }, error => {
+      response.status(500).send(error);
+    });
   });
-
-
 };
 
 module.exports = appendRoutes;
---
+++ routes/helpers/article.js
@@ -0,0 +1,57 @@
+"use strict";
+
+function getArticle(knex, id, callback, errHandler) {
+  let articleObject = {};
+
+  const articleQuery = knex
+    .select('*')
+    .from('articles')
+    .where('id', id)
+    .orderBy('created_at');
+
+  const sectionsQuery = knex
+    .select('*')
+    .from('sections')
+    .where('article_id', id)
+    .orderBy('id');
+
+  const tagsQuery = knex
+    .select('tags.*')
+    .from('article_tags')
+    .join('tags', 'tags.id', 'article_tags.tag_id')
+    .where('article_tags.article_id', id)
+    .orderBy('tags.id');
+
+  const sectionCommentsQuery = function(sectionId) {
+    return knex
+      .select('comments.*')
+      .from('comments')
+      .where('section_id', sectionId)
+      .orderBy('created_at');
+  };
+
+  const authorQuery = knex
+    .select('users.*')
+    .from('users').join('articles', 'users.id', 'articles.user_id')
+    .where('articles.id', id);
+
+  return articleQuery
+    .then(articles => articleObject = articles[0])
+    .catch(error => errHandler(error))
+    .then(() =>
+      sectionsQuery.then(sections => {
+        articleObject.sections = sections;
+        return Promise.all(sections.map(section => {
+          return sectionCommentsQuery(section.id)
+            .then(comments => section.comments = comments);
+        }));
+      }))
+    .catch(error => errHandler(error))
+    .then(() => tagsQuery.then(tags => { articleObject.tags = tags; }))
+    .catch(error => errHandler(error))
+    .then(() => callback(articleObject))
+    .catch(error => errHandler(error));
+
+};
+
+module.exports = getArticle;
