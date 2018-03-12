472cdd4615e0337d40e20da021b8ca43aa594a01
vagrant <spencer.greff@gmail.com>
2018-03-12 22:02:34 +0000

    Styling and article list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,4 +13,14 @@ $(function() {
     })
   }
   const articles = loadArticles();
+
+  $('.popover-markup>.trigger').popover({
+    html: true,
+    title: function() {
+      return $(this).parent().find('.head').html();
+    },
+    content: function() {
+      return $(this).parent().find('.content').html();
+    }
+  });
 })
