1d4cd7ca5b576f3f89e72cf67dc5db57fe0ccdb5
Joel Tannas <jtannas@gmail.com>
2018-03-13 16:08:47 -0600

    add schema for voting on comments

--- db/migrations/20180313154031_create_comment_votes.js
+++ db/migrations/20180313154031_create_comment_votes.js
@@ -1,19 +1,20 @@
 exports.up = async function(knex, Promise) {
-  return knex.schema.createTable("comments", function(table) {
+  return knex.schema.createTable("comment_votes", function(table) {
     table.increments('id').primary();
-    table.text('content').notNullable();
-    table.boolean('agree').notNullable();
-    table.integer('section_id').unsigned().notNullable();
+    table.integer('comment_id').notNullable();
     table.text('user_id').notNullable();
-    table.integer('initial_score').notNullable().defaultTo(0);
+    table.boolean('is_upvote');
+
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
 
+    table.foreign('comment_id').references('comments.id');
     table.foreign('user_id').references('users.id');
-    table.foreign('section_id').references('sections.id');
+
+    table.unique(['comment_id', 'user_id']);
   });
 };
 
 exports.down = async function(knex) {
-  await knex.schema.dropTable("comments");
+  await knex.schema.dropTable("comment_votes");
 };
