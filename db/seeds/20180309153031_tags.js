0a0f1322c105c46f1e773718bc0762c8bc59fd91
Joel Tannas <jtannas@gmail.com>
2018-03-13 12:16:44 -0600

    improve seedfile for tags

--- db/seeds/20180309153031_tags.js
+++ db/seeds/20180309153031_tags.js
@@ -3,10 +3,39 @@ exports.seed = function(knex, Promise) {
   return knex('tags').del()
     .then(function() {
       // Inserts seed entries
-      return knex('tags').insert([
-        { id: 1, display_name: 'tag_name_1', url_slug: 'url_slug_1' },
-        { id: 2, display_name: 'tag_name_2', url_slug: 'url_slug_2' },
-        { id: 3, display_name: 'tag_name_3', url_slug: 'url_slug_3' }
-      ]);
+      return knex('tags').insert([{
+        "id": 1,
+        "display_name": "Acoustic Guitar",
+        "url_slug": "acoustic_guitar"
+      }, {
+        "id": 2,
+        "display_name": "SAP EBP",
+        "url_slug": "sap_ebp"
+      }, {
+        "id": 3,
+        "display_name": "Force Protection",
+        "url_slug": "force_protection"
+      }, {
+        "id": 4,
+        "display_name": "Norton Ghost",
+        "url_slug": "norton_ghost"
+      }, {
+        "id": 5,
+        "display_name": "Illustrator",
+        "url_slug": "illustrator"
+      }, {
+        "id": 6,
+        "display_name": "CFOs",
+        "url_slug": "cfos"
+      }, {
+        "id": 7,
+        "display_name": "IICRC Certified",
+        "url_slug": "iicrc_certified"
+      }, {
+        "id": 8,
+        "display_name": "Power Plants",
+        "url_slug": "power_plants"
+      }]);
+
     });
 };
