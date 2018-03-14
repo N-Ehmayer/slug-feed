2ee5b6e032a32c619f9ae1a85a6abcbb609f42d5
vagrant <spencer.greff@gmail.com>
2018-03-14 22:01:20 +0000

    Navbar Partial fixed compile

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
 import logo from '../logo.svg'
 import { connect } from 'react-redux'
-import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import NavbarFeatures from './components/_navbar_features.jsx'
+import NavbarFeatures from './_navbar_features.jsx'
 
 class App extends Component {
   render() {
     return (
-      <Router>
         <NavbarFeatures />
-      </Router>
+
     );
   }
 }
--- client/src/components/_navbar_features.jsx
+++ client/src/components/_navbar_features.jsx
@@ -2,51 +2,25 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-class NavbarFeatures extends React.Component {
-		constructor(props) {
-				super(props);
-				this.state = {
-						collapse: false,
-						isWideEnough: false,
-						dropdownOpen: false
-				};
-		this.onClick = this.onClick.bind(this);
-		this.toggle = this.toggle.bind(this);
-		}
-
-		onClick(){
-				this.setState({
-						collapse: !this.state.collapse,
-				});
-		}
-
-		toggle() {
-				this.setState({
-						dropdownOpen: !this.state.dropdownOpen
-				});
-		}
-
-		render() {
-				return (
-						<Router>
-								<Navbar color="indigo" dark expand="md" scrolling>
-								  <div className="row">
-											 <NavbarNav className="mx-auto">
-												  <NavItem>
-													   <NavLink className="nav-link" href="/">Slug Feed</NavLink>
-												  </NavItem>
-												  <NavItem>
-														  <NavLink className="nav-link" to="#">Pricing</NavLink>
-												  </NavItem>
-														<form className="form-inline">
-														  <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-														</form>
-												</NavbarNav>
-										</div>
-								</Navbar>
-						</Router>
-				);
-		}
+class NavbarFeatures extends Component {
+    render() {
+        return (
+            <Router>
+                <Navbar color="indigo" dark expand="md" scrolling>
+                  <div className="row">
+                       <NavbarNav className="mx-auto">
+                          <NavItem>
+                          <p>Slug Feed</p>
+                          </NavItem>
+                          <NavItem>
+                          <p>Login/Logout</p>
+                          </NavItem>
+                        </NavbarNav>
+                    </div>
+                </Navbar>
+            </Router>
+        );
+    }
 }
 
 export default NavbarFeatures
