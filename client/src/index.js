ad8f0e9d6f850639ae6b9fe83272efc82120a146
N-Ehmayer <nic.ehmayer92@gmail.com>
2018-03-14 15:37:41 -0600

    Added Home and ShowArticle routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store.js'
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from './components/Home.jsx'
+import ShowArticle from './components/ShowArticle.jsx'
 
 ReactDOM.render(
   <Router>
     <Provider store={store}>
-      <App />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/article/1" component={ShowArticle} />
+      </Switch>
     </Provider>
   </Router>
 , document.getElementById('root'));
