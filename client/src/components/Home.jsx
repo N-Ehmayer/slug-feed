420fa22104083b5cb31d6d0ae5966e4e29843b2c
Nic Ehmayer <nic.ehmayer92@gmail.com>
2018-03-15 11:04:21 -0600

    Setup intial actions and reducers folders (#48)

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,25 +2,40 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import NavbarFeatures from './NavbarFeatures'
+import Card from './Card'
 
 import ShowArticle from './ShowArticle.jsx'
 
+import { fetchArticles } from '../actions/articleActions.js'
+
+
+
 class Home extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(fetchArticles());
+  }
+
   render() {
+    const { articles } = this.props
     return (
       <div className="App">
         <NavbarFeatures />
 
         <h1>Home Route</h1>
         <Link to="/article/1">ShowArticle</Link>
+
+        <Card articles={articles}/>
+
       </div>
     );
   }
 }
 
-export default connect(
-  state=>({
+const mapStateToProps = (store) => {
+  return {
+    articles: store.articles.articles
+  }
+}
 
-  }),
-  {}
-)(Home)
+export default connect(mapStateToProps)(Home)
