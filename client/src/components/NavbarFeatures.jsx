eaf47f9d71d41e3941c1b9d6ee7ae93e5cbf9104
Joel Tannas <jtannas@gmail.com>
2018-03-15 21:02:53 -0600

    restyle navbar & tie to user session

--- client/src/components/NavbarFeatures.jsx
+++ client/src/components/NavbarFeatures.jsx
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
+import { connect } from 'react-redux';
+import { Navbar, NavbarBrand, NavbarNav, NavItem } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const mapStateToProps = (state) => ({
+   session: state.session
+});
+
 
 class NavbarFeatures extends Component {
-    render() {
-        return (
-            <Router>
-                <Navbar color="indigo" dark expand="md" scrolling>
-                  <div className="row">
-                       <NavbarNav className="mx-auto">
-                          <NavItem>
-                          <p>Slug Feed</p>
-                          </NavItem>
-                          <NavItem>
-                          <p>Login/Logout</p>
-                          </NavItem>
-                        </NavbarNav>
-                    </div>
-                </Navbar>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Navbar light main-nav expand="md" scrolling>
+          <NavbarNav className="ml-auto">
+            <NavItem><Link className="nav-link" to="/">Home</Link></NavItem>
+            <NavItem><NavbarBrand href="/" className="navbar-logo mx-auto">SlugFeed</NavbarBrand></NavItem>
+            {this.props.session.user
+              ? <NavItem><a className="nav-link" href="/logout">Logout</a></NavItem>
+              : <NavItem><a className="nav-link" href="/login">Login</a></NavItem>
+            }
+          </NavbarNav>
+        </Navbar>
+      </Router>
+    );
+  }
 }
 
-export default NavbarFeatures
+export default connect(mapStateToProps)(NavbarFeatures);
