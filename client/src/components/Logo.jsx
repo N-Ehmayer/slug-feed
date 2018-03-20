c2fafed4afb88bc216af559fbe08197cd6eb1ccc
Spencer Greff <spencer.greff@gmail.com>
2018-03-20 13:34:41 -0600

    added logo to navbar. Logo files are in src/img. Multiple pngs for different use cases. Vector source saved to /docs

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-
+import logo from '../img/logo-black-shift.png';
 class Logo extends Component {
   render() {
     return (
-            <h2>SlugFeed</h2>
+          <div className="mx-auto">
+            <img width="300px" src={logo} />
+          </div>
+
     );
   }
 }
--- client/src/components/NavbarFeatures.jsx
+++ client/src/components/NavbarFeatures.jsx
@@ -14,13 +14,13 @@ class NavbarFeatures extends Component {
     return (
       <Navbar light className="to-top" expand="md" fixed="top" scrolling>
         <NavbarNav className="ml-auto">
-          <NavItem><NavLink className="nav-link" to="/">Feed</NavLink></NavItem>
+          <NavItem><NavLink className="nav-link nav-hover-red" to="/">Feed</NavLink></NavItem>
           <NavItem><NavbarBrand href="/" className="navbar-logo mx-auto">
             <Logo />
           </NavbarBrand></NavItem>
           {(this.props.session.user || {}).id
-            ? <NavItem><a className="nav-link" href="/logout">Logout</a></NavItem>
-            : <NavItem><a className="nav-link" href="/login">Login</a></NavItem>
+            ? <NavItem><a className="nav-link nav-hover-blue" href="/logout">Logout</a></NavItem>
+            : <NavItem><a className="nav-link nav-hover-blue" href="/login">Login</a></NavItem>
           }
         </NavbarNav>
       </Navbar>
