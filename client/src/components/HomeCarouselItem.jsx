e228fb0a2b50910aa82caef3f7696bb967ef5b4f
Joel Tannas <jtannas@gmail.com>
2018-03-20 13:21:10 -0600

    made transitions less jarring

--- client/src/components/HomeCarouselItem.jsx
+++ client/src/components/HomeCarouselItem.jsx
@@ -5,7 +5,7 @@ import { CarouselItem, CarouselCaption } from 'mdbreact';
 const HomeCarouselItem = props => {
   return (
     <CarouselItem itemId={props.itemId}>
-      <div className="view hm-black-light">
+      <div className="view hm-black-strong">
         <img className="d-block w-100" style={{objectFit: 'cover'}} height='400px'src={props.article.hero_img_url}/>
         <div className="mask"></div>
       </div>
