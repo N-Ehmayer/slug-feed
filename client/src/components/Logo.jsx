09e89b736711996a1a5bf13538656ecd2973fe9a
Spencer Greff <spencer.greff@gmail.com>
2018-03-20 22:53:53 -0600

    merge fix

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from '../img/logo-black-shift.png';
+import logo from '../img/logo-red-shift.png';
 class Logo extends Component {
   render() {
     return (
-          <div className="mx-auto">
+          <div className="mx-auto logo">
             <img width="300px" src={logo} alt='logo'/>
           </div>
 
